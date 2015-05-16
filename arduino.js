5092e45c7ad00999a3ea2ea6fae8ed2aba4bc062
Matt <sixteenmillimeter@riseup.com>
2015-05-16 08:59:36 -0400

    Test serialport/express setup

--- arduino.js
+++ arduino.js
@@ -2,6 +2,9 @@ var arduino = {},
 	serialport = require('serialport'),
 	express = require('express'),
 	app = express();
-
+mcopy.arduino.serial = new SerialPort(mcopy.arduino.path, {
+  baudrate: mcopy.cfg.arduino.baud,
+  parser: sp.parsers.readline("\n")
+});
 
 app.listen(9555);
