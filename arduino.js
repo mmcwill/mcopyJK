34f82e01bcd309ddfa5e29f562933ead7c747d4f
Matt <sixteenmillimeter@riseup.net>
2015-06-26 15:17:51 -0400

    Add fs and first endpoint

--- arduino.js
+++ arduino.js
@@ -1,4 +1,5 @@
 var arduino = {},
+	fs = require('fs'),
 	serialport = require('serialport'),
 	express = require('express'),
 	app = express(),
@@ -7,10 +8,16 @@ var arduino = {},
 arduino.cfgFile = './data/cfg.json';
 arduino.cfg = JSON.parse(fs.readFileSync(arduino.cfgFile, 'utf8'));
 
-
 arduino.serial = new serialport('/dev/ttyACM0', {
   baudrate: arduino.cfg.arduino.baud,
   parser: sp.parsers.readline("\n")
 });
+
+app.get('/connect', function (req, res) {
+	var output = {
+		success: false
+	}
+	res.json(output);
+})
 
 app.listen(9555);
