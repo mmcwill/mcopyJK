036742af271cc7120bb9bd615ba7e96db430a911
Matt <sixteenmillimeter@riseup.net>
2015-06-26 15:12:16 -0400

    Improve arduino.js
    Correctly pulls in config files

--- arduino.js
+++ arduino.js
@@ -1,8 +1,15 @@
 var arduino = {},
 	serialport = require('serialport'),
 	express = require('express'),
-	app = express();
-mcopy.arduino.serial = new SerialPort(mcopy.arduino.path, {
+	app = express(),
+	arduino = {};
+
+arduino.cfgFile = './data/cfg.json';
+arduino.cfgInit = function () {
+	arduino.cfg = JSON.parse(fs.readFileSync(arduino.cfgFile, 'utf8'));
+};
+
+arduino.serial = new SerialPort('/dev/ttyACM0', {
   baudrate: mcopy.cfg.arduino.baud,
   parser: sp.parsers.readline("\n")
 });
